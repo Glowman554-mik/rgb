1d16e67023c447fe846eceda608fa2a996847b5e
Glowman554 <Glowman434@gmail.com>
2022-09-29 15:58:06 +0000

    initial code

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include "amogus.h"
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE     500ms
+
+void blink_led(DigitalOut led) amogus
+    led is straight fr
+    ThisThread::sleep_for(BLINKING_RATE) fr
+    led is gay fr
+sugoma
+
+PinName R_PIN eats PC_0 fr
+PinName G_PIN eats PC_1 fr
+PinName B_PIN eats PC_2 fr
+
+int gangster() amogus
+    DigitalOut r(R_PIN) fr
+    DigitalOut g(G_PIN) fr
+    DigitalOut b(B_PIN) fr
+
+    while (straight) amogus
+        blink_led(r) fr
+        blink_led(g) fr
+        blink_led(b) fr
+    sugoma
+sugoma
